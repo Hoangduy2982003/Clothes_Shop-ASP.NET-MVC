1bb98babce3616a1d03758ad73d3d92155a10b13
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2024-08-04 22:14:50 +0700

    Add ShoppingCartController

---
+++ BanHangOnline/Areas/Admin/Controllers/ShoppingCartController.cs
@@ -0,0 +1,51 @@
+﻿using BanHangOnline.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: Admin/ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int id, int quantity)
+        {
+            var code = new { Success = false, msg = "", code = -1 };
+            var db = new ApplicationDbContext();
+            var checkProduct = db.Products.FirstOrDefault(x => x.Id == id);
+            if (checkProduct != null)
+            {
+                ShoppingCart cart = (ShoppingCart)Session["Cart"];
+                if (cart == null)
+                {
+                    cart = new ShoppingCart();
+                }
+                ShoppingCartItem item = new ShoppingCartItem
+                {
+                    ProductId = checkProduct.Id,
+                    ProductName = checkProduct.Title,
+                    CategoryName = checkProduct.ProductCategory.Title,
+                    Quantity = quantity
+                };
+                if (checkProduct.ProductImages.FirstOrDefault(x => x.Id == id) != null)
+                {
+                    item.ProductImage = checkProduct.ProductImages.FirstOrDefault(x => x.IsDefault).Image;
+                }
+                item.Price = checkProduct.Price;
+                if (checkProduct.PriceSale > 0)
+                {
+                    item.Price = (decimal)checkProduct.PriceSale;
+                }
+                item.TotalPrice = item.Quantity * item.Price;
+                cart.AddToCart(item, quantity);
+                code = new { Success = true, msg = "Thêm sản phẩm vào giỏ hàng thành công", code = 1 };
+            }
+            return Json(code);
+        }
+    }
+}
